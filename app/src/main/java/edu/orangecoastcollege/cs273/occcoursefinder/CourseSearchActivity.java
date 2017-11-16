ed7ac59da9b464e6245342ba51dd491c6764ad20
Derek <dphtran501@yahoo.com>
2017-11-16 15:24:32 -0800

    Add search bar list filter

--- app/src/main/java/edu/orangecoastcollege/cs273/occcoursefinder/CourseSearchActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/occcoursefinder/CourseSearchActivity.java
@@ -2,6 +2,8 @@ package edu.orangecoastcollege.cs273.occcoursefinder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -43,6 +45,7 @@ public class CourseSearchActivity extends AppCompatActivity {
         allCoursesList = db.getAllCourses();
 
         courseTitleEditText = (EditText) findViewById(R.id.courseTitleEditText);
+        courseTitleEditText.addTextChangedListener(courseTitleTextWatcher);
         instructorSpinner = (Spinner) findViewById(R.id.instructorSpinner);
 
 
@@ -59,21 +62,22 @@ public class CourseSearchActivity extends AppCompatActivity {
 
     }
 
+    // Create a method getInstructorNames that returns a String[] containing the entry
+    // "[SELECT INSTRUCTOR]" at position 0, followed by all the full instructor names in the
+    // allInstructorsList
     private String[] getInstructorNames()
     {
         String[] instructorNames = new String[allInstructorsList.size() + 1];
         instructorNames[0] = "[Select Instructor]";
         for (int i = 1; i < instructorNames.length; i++)
-        {
             instructorNames[i] = allInstructorsList.get(i - 1).getFullName();
-        }
 
         return instructorNames;
     }
 
-    // Create a method getInstructorNames that returns a String[] containing the entry
-    // "[SELECT INSTRUCTOR]" at position 0, followed by all the full instructor names in the
-    // allInstructorsList
+    // Create a void method named reset that sets the test of the edit text back to an
+    // empty string, sets the selection of the Spinner to 0 and clears out the offeringListAdapter,
+    // then rebuild it with the allOfferingsList
     public void reset(View v)
     {
         // Set spinner back
@@ -85,19 +89,35 @@ public class CourseSearchActivity extends AppCompatActivity {
         offeringListAdapter.addAll(allOfferingsList);
     }
 
-    //TODO (3): Create a void method named reset that sets the test of the edit text back to an
-    //TODO: empty string, sets the selection of the Spinner to 0 and clears out the offeringListAdapter,
-    //TODO: then rebuild it with the allOfferingsList
+    // Create a TextWatcher named courseTitleTextWatcher that will implement the onTextChanged method.
+    // In this method, set the selection of the instructorSpinner to 0, then
+    // Clear the offeringListAdapter
+    // If the entry is an empty String "", the offeringListAdapter should addAll from the allOfferingsList
+    // Else, the offeringListAdapter should add each Offering whose course title starts with the entry.
 
+    public TextWatcher courseTitleTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+        {
+            String entry = charSequence.toString().trim().toUpperCase();
+            // Clear the list adapter
+            offeringListAdapter.clear();
+            // Let's loop through Offerings
+            for (Offering o : allOfferingsList)
+            {
+                // If the course title starts with entry, add it back to the list adapter
+                if (o.getCourse().getTitle().toUpperCase().startsWith(entry))
+                    offeringListAdapter.add(o);
+            }
 
-    //TODO (4): Create a TextWatcher named courseTitleTextWatcher that will implement the onTextChanged method.
-    //TODO: In this method, set the selection of the instructorSpinner to 0, then
-    //TODO: Clear the offeringListAdapter
-    //TODO: If the entry is an empty String "", the offeringListAdapter should addAll from the allOfferingsList
-    //TODO: Else, the offeringListAdapter should add each Offering whose course title starts with the entry.
+        }
 
-
+        @Override
+        public void afterTextChanged(Editable editable) {}
+    };
 
     //TODO (5): Create an AdapterView.OnItemSelectedListener named instructorSpinnerListener and implement
     //TODO: the onItemSelected method to do the following:
